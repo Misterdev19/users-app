d167795c9a5fa73dc00c43e868fb64ae63cacd0d
Misterdev19 <jcpenagos716@outlook.com>
2024-08-29 22:46:55 -0500

    frontend whit bootstrap

--- index.js
+++ index.js
@@ -1,15 +1,49 @@
-const URL_RQUEST  = "https://jsonplaceholder.typicode.com/users";
 
-fetch(URL_RQUEST).then(function(response){
-      if(response.status == 404 ){
-          return new Error("No existe el recurso");
-      }
-       
-      return response.json();
-    
-}).then(function(data){
-        console.log(data)
+
+function componentRowUser(user){
+
+    return `<tr>
+            <th class="text-center" scope="row">${user.name}</th>
+            <td class="text-center">${user.username}</td>
+            <td class="text-center">${user.email}</td>
+            <td class="text-center">${user.phone}</td>
+        </tr>`;  
+}
+
+
+function  viewsUsersList(dataUser){
+
+    const tableBody  = document.getElementById('dom_table_user');
+    let rowUser  = "";
+    dataUser.forEach( function (user){
+
+        rowUser += componentRowUser(user);
         
-}).catch(function(response){
-    console.log(response)
-});
+    });
+    
+    tableBody.innerHTML = rowUser;
+}
+                                                  
+
+function fetchApi(){
+        const URL_RQUEST  = "https://jsonplaceholder.typicode.com/users";
+        const domLoad  = document.getElementById('dom_table_user');
+        domLoad.innerHTML = `<h1>Cargando</h1>`;
+        fetch(URL_RQUEST).then(function(response){
+            if(response.status == 404 ){
+                return new Error("No existe el recurso");
+            }
+        
+            return response.json();
+            
+        }).then(function(data){
+
+            viewsUsersList(data)
+                
+        }).catch(function(response){
+            console.log(response)
+        });
+}
+
+
+fetchApi();
